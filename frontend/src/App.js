0d2443f502ffae67c257a122942fbbdb1c9e9f91
Ankit Kushwaha <ankitkushwaha26548@gmail.com>
2026-03-30 15:30:25 +0530

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <H1>Welcome to the Helpdesk System</H1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
